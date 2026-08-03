5c3e3e7b9e66a89aef8368c30680e2b3c1aa2659
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2026-08-03 23:21:10 +0600

    Initial Commit

---
+++ ClipboardManager/ClipboardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardManagerApp.swift
+//  ClipboardManager
+//
+//  Created by Ashraful Mijan on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
